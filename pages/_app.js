f778548ed566cccb776deaa871275ced22bbc651
Nafid Faham <nafidfaham08@gmail.com>
2022-09-13 18:25:08 +0600

    Add more meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,30 @@ function MyApp({ Component, pageProps }) {
           name="description"
           content="Simple Weather App Made with NextJS"
         />
-        <meta itemProp="image" content="/background.jpg" />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://weather-app-beta-livid.vercel.app/"
+        />
+        <meta property="og:title" content="Next Weather" />
+        <meta
+          property="og:description"
+          content="Simple Weather App Made with NextJS"
+        />
+        <meta property="og:image" content="background.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://weather-app-beta-livid.vercel.app/"
+        />
+        <meta property="twitter:title" content="Next Weather" />
+        <meta
+          property="twitter:description"
+          content="Simple Weather App Made with NextJS"
+        />
+        <meta property="twitter:image" content="background.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
