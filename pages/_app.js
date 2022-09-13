e5501423a34d1a6e28fe2fab863444eb4f349d33
Nafid Faham <nafidfaham08@gmail.com>
2022-09-13 18:17:51 +0600

    Add some meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Next Weather</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Simple Weather App Made with NextJS"
+        />
+        <meta itemProp="image" content="/background.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
